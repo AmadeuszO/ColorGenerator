6f3b1b3ca285670e2a558ecf0e82cad32fa15c6b
Amadeusz Ozog <ozog@amadeuszozog.pl>
2023-08-06 13:54:36 +0200

    Color Generator

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-const app = alert('Działa');
+const gradientBox = document.querySelector('.gradient-box');
+const selectMenu = document.querySelector('.select-box select');
+const colorInput = document.querySelectorAll('.colors input');
+const textarea = document.querySelector('textarea')
+
+const generateGradient = () => {
+    const gradient = `linear-gradient(to top left,(${selectMenu.value}, ${colorInput[0].value}, ${colorInput[1].value})`;
+    gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient};`;
+}
+
+colorInput.forEach(input => {
+    input.addEventListener('input', generateGradient)
+});
+
+selectMenu.addEventListener('change', generateGradient);
